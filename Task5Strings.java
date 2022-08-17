04f6dc273f33e372321efd17205f665d0e5f31d3
ivsbk <103572344+ivsbk@users.noreply.github.com>
2022-08-17 12:40:44 +0300

    Update Task5Strings.java

--- Task5Strings.java
+++ Task5Strings.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class Task5Strings {
     public static void main(String[] args) {
+
         int i, seedLength;
-        String seed, nextSeed;
+        String seed, nextSeed, linePow;
         boolean rootCheck = true;
+        final int maxLength = 1023;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите строку");
+        System.out.println("Введите строку до 100 символов");
         String sourceLine = scanner.nextLine();
         String line = sourceLine.toLowerCase();
 
-        System.out.println("Введите число. Положительное возведёт строку в степень, отрицательное — вычислит корень.");
+        System.out.println("Введите число меньше 100001. Положительное возведёт строку в степень, отрицательное — вычислит корень.");
         int pow = scanner.nextInt();
 
         scanner.close();
 
         if (pow > 0) {
-            for (i = 1; i <= pow; i++) System.out.print(line);
+            linePow = line.repeat(pow);
+            if (linePow.length() > maxLength) System.out.print(linePow.substring(0,maxLength - 1));
+            else System.out.print(linePow);
+
         } else if (pow < 0) {
             seedLength = line.length() / Math.abs(pow);
             seed = line.substring(0,seedLength);
@@ -32,7 +37,10 @@ public class Task5Strings {
                }
            }
             if (rootCheck == false) System.out.println("Корень невычислим");
-            else System.out.println(seed);
+            else {
+                if (seed.length() > maxLength) System.out.print(seed.substring(0,maxLength - 1));
+                else System.out.print(seed);
+            }
 
         } else if (Math.abs(pow) > line.length() && pow < 0) {
            System.out.println("Корень невычислим");
